dc7a8912f3f348d09fc13199c45d11fb37c91b9e
ppoudel <ppoudel@IN-MUM-LDVM029.corp.capgemini.com>
2018-10-08 09:26:10 -0400

    commit

---
+++ src/main/java/com/capgemini/productapp/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.capgemini.productapp.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.capgemini.productapp.entity.Product;
+
+public interface ProductRepository extends MongoRepository<Product, Integer>{
+
+}
